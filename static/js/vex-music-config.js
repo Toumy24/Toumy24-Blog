f793260f19688266212ade3e4ce65b26d42f14d6
Toumy24 <2114481539@qq.com>
2026-04-16 07:12:58 +0800

    debug

--- static/js/vex-music-config.js
+++ static/js/vex-music-config.js
@@ -6,6 +6,6 @@ export const MUSIC_LAYOUT_CONFIG = {
   baseStaveWidth: 250,   // 基础小节宽度
   measuresPerLine: 4,    // 每行小节数
   lineHeight: 87,        // 行间距
-  clefSpaceWidth: 0,   // 谱号和拍号占用的额外宽度
+  clefSpaceWidth: 80,   // 谱号和拍号占用的额外宽度
   padding: 10,           // 画布内边距
 };
